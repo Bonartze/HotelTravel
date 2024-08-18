0c5bf55dcfe20188e346475db26aac9ca4046b09
Bonartze <iokirilin@mail.ru>
2024-08-18 22:16:09 +0300

    fixed pipeline

--- Telegram/Calculation_Server/Calculations.h
+++ Telegram/Calculation_Server/Calculations.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <any>
 #include <string>
-#include </../../third_party/json/include/nlohmann/json.hpp>
+#include <nlohmann/json.hpp>
 #include <fstream>
 #include <cmath>
 
